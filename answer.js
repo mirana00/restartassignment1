e7d7482ed6fd638d846144edada431467850d318
mirana00 <miranahanif@gmail.com>
2026-09-07 22:53:53 +0600

    completed q 4

--- answer.js
+++ answer.js
@@ -49,3 +49,27 @@ function validateUsername(username){
 }
 
 
+// Question 4: Dhaka CNG Fare Meter
+
+function getCngFare(distance, isNight= false, waitingMinutes= 0){
+    let fare = 50;
+
+    if(distance> 2){
+        fare = fare + (distance - 2)* 15;
+    }
+    fare = fare + waitingMinutes* 2;
+
+    if(isNight){
+        fare = fare * 1.20;
+    }
+
+    return fare;
+}
+
+console.log(getCngFare(2));
+console.log(getCngFare(1));
+console.log(getCngFare(5));
+console.log(getCngFare(10));
+console.log(getCngFare(5, false, 10));
+console.log(getCngFare(5, true));
+console.log(getCngFare(5, true, 10));
